fdcc6b0d28b01acb0c799eab803bc49c99c8371a
Robert Lee-Cann <leeky@leeky.org.uk>
2018-07-20 11:09:13 +0100

    Grab calendars for the rooms

--- test.rb
+++ test.rb
@@ -8,6 +8,12 @@ OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
 TOKEN_PATH = 'token.yaml'.freeze
 SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
 
+calendars = [
+  { name: 'The Hide', id: 'dxw.com_3936393930353336393539@resource.calendar.google.com' },
+  { name: 'Ground Floor Meeting Room', id: 'dxw.com_2d36303034323634352d353334@resource.calendar.google.com' },
+  { name: 'Wellbeing Room', id: 'dxw.com_3437393236383531353437@resource.calendar.google.com' }
+]
+
 def authorize
   client_id = Google::Auth::ClientId.new(ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'])
   token_store = Google::Auth::Stores::FileTokenStore.new(file: TOKEN_PATH)
@@ -31,16 +37,20 @@ service = Google::Apis::CalendarV3::CalendarService.new
 service.client_options.application_name = 'Room Usage Dashboard'
 service.authorization = authorize
 
-# Fetch the next 10 events for the user
-calendar_id = 'primary'
-response = service.list_events(calendar_id,
-                               max_results: 10,
-                               single_events: true,
-                               order_by: 'startTime',
-                               time_min: Time.now.iso8601)
-puts 'Upcoming events:'
-puts 'No upcoming events found' if response.items.empty?
-response.items.each do |event|
-  start = event.start.date || event.start.date_time
-  puts "- #{event.summary} (#{start})"
+# Fetch the next 2 events for this room
+calendars.each do |calendar|
+  puts "#{calendar[:name]}:"
+
+  response = service.list_events(calendar[:id],
+                                 max_results: 2,
+                                 single_events: true,
+                                 order_by: 'startTime',
+                                 time_min: Time.now.iso8601)
+  puts 'Upcoming events:'
+  puts 'No upcoming events found' if response.items.empty?
+  response.items.each do |event|
+    start = event.start.date || event.start.date_time
+    puts "- #{event.summary} (#{start})"
+  end
+  puts
 end
