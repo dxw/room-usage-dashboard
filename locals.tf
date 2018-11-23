e7c498028847ca0ed629758720f1c8660d462508
Chris Wright <chris@dxw.com>
2018-11-23 13:42:24 +0000

    (Feature) Build Pipeline
    * Creates a CodePipeline resource to build and deploy the app
    * Adds a buildspec.yml to run tests and create the docker image in ECR
    * Creates a template `docker-entrypoint.sh`

---
+++ locals.tf
@@ -0,0 +1,5 @@
+locals {
+  app_github_owner = "dxw"
+  app_github_repo  = "room-usage-dashboard"
+  app_service_name = "app"
+}
--- variables.tf
+++ variables.tf
@@ -9,3 +9,19 @@ variable "dalmatian_role" {
 variable "account_id" {
   description = "AWS Account ID"
 }
+
+variable "environment" {
+  description = "Environment"
+}
+
+variable "track_revision" {
+  description = "Git branch to track"
+}
+
+variable "app_github_token" {
+  description = "App GitHub Token"
+}
+
+variable "cluster_name" {
+  description = "Cluster Name"
+}
