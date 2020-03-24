d2cc9fa08410a0eca265070fdef9e54bb188a1ba
Chris Wright <chris@dxw.com>
2020-03-24 14:19:58 +0000

    (Chore) Remove terraform

--- locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  app_github_owner = "dxw"
-  app_github_repo  = "room-usage-dashboard"
-  app_service_name = "app-${var.environment}"
-
-  app_container_entrypoint = ["./docker-entrypoint.sh"]
-  app_container_command    = ["rackup", "--host", "0.0.0.0"]
-}
