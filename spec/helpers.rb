1eabf18aadd70315ee5a375b1eaa1df1413472cf
Maria Herrera <maria@dxw.com>
2020-07-24 12:12:01 +0100

    Tests for the API
    This commit introduces a test suite for the API side of the app. I created a
    Helpers module to include the methods used in the API tests as I wanted them to
    be available for the user-facing app tests too.
    
    These tests showed an error in the code when it comes to reject meetings with
    no attendees or when all attendees have declined. I fixed the error in
    `lib/room.rb` and now the tests pass.

---
+++ spec/helpers.rb
@@ -0,0 +1,125 @@
+module Helpers
+  def gcal_fake_response(items)
+    {
+      "kind" => "calendar#events",
+      "etag" => "test etag",
+      "summary" => "test summary",
+      "description" => "test description",
+      "items" => items
+    }.to_json
+  end
+
+  def gcal_fake_events_with_attendees
+    [{
+      "kind" => "calendar#event",
+      "etag" => "sales etag",
+      "id" => "sales test id",
+      "summary" => "sales - pipeline",
+      "description" => "sales meeting",
+      "organizer" => {
+        "email" => "sales@example.com",
+        "displayName" => "Sales Person"
+      },
+      "start" => {
+        "dateTime" => "2020-07-20T15:00:00+01:00"
+      },
+      "end" => {
+        "dateTime" => "2020-07-20T15:30:00+01:00"
+      },
+      "attendees" => [
+        {
+          "email" => "attendee01@example.com",
+          "displayName" => "Attendee 01",
+          "responseStatus" => "accepted"
+        },
+        {
+          "email" => "attendee02@example.com",
+          "displayName" => "Attendee 02",
+          "responseStatus" => "accepted"
+        }
+      ]
+    }, {
+      "kind" => "calendar#event",
+      "etag" => "sales planning etag",
+      "id" => "sales planning test id",
+      "summary" => "sales - forward planning",
+      "description" => "sales planning meeting",
+      "organizer" => {
+        "email" => "sales@example.com",
+        "displayName" => "Sales Person"
+      },
+      "start" => {
+        "dateTime" => "2020-07-20T15:30:00+01:00"
+      },
+      "end" => {
+        "dateTime" => "2020-07-20T16:00:00+01:00"
+      },
+      "attendees" => [
+        {
+          "email" => "attendee01@example.com",
+          "displayName" => "Attendee 01",
+          "responseStatus" => "accepted"
+        },
+        {
+          "email" => "attendee02@example.com",
+          "displayName" => "Attendee 02",
+          "responseStatus" => "accepted"
+        }
+      ]
+    }]
+  end
+
+  def gcal_fake_events_without_attendees
+    [{
+      "kind" => "calendar#event",
+      "etag" => "sales etag",
+      "id" => "sales test id",
+      "summary" => "sales - pipeline",
+      "description" => "sales meeting",
+      "organizer" => {
+        "email" => "sales@example.com",
+        "displayName" => "Sales Person"
+      },
+      "start" => {
+        "dateTime" => "2020-07-20T15:00:00+01:00"
+      },
+      "end" => {
+        "dateTime" => "2020-07-20T15:30:00+01:00"
+      },
+      "attendees" => []
+    }, {
+      "kind" => "calendar#event",
+      "etag" => "sales planning etag",
+      "id" => "sales planning test id",
+      "summary" => "sales - forward planning",
+      "description" => "sales planning meeting",
+      "organizer" => {
+        "email" => "sales@example.com",
+        "displayName" => "Sales Person"
+      },
+      "start" => {
+        "dateTime" => "2020-07-20T15:30:00+01:00"
+      },
+      "end" => {
+        "dateTime" => "2020-07-20T16:00:00+01:00"
+      },
+      "attendees" => [
+        {
+          "email" => "attendee01@example.com",
+          "displayName" => "Attendee 01",
+          "responseStatus" => "declined"
+        },
+        {
+          "email" => "attendee02@example.com",
+          "displayName" => "Attendee 02",
+          "responseStatus" => "declined"
+        }
+      ]
+    }]
+  end
+
+  def stub_gcal_request(body)
+    stub_request(:any, /www.googleapis.com/)
+      .to_return(body: body, headers: {"Content-Type" => "application/json"})
+  end
+end
